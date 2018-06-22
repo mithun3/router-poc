7d518df5baa3caff48130ed0efa3f9a6120503cc
P S <617150@cognizant.com>
2018-06-22 13:17:04 +1000

    initial commit - spring boot initializer

---
+++ src/main/java/com/router/poc/RouterPocApplication.java
@@ -0,0 +1,12 @@
+package com.router.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouterPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouterPocApplication.class, args);
+	}
+}
